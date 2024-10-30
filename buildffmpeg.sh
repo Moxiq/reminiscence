2e0d981a50cd3b2a9cfec5a813da5ab954f3024f
arthur <tehtaq@gmail.com>
2024-10-30 23:46:13 +0100

    ah

--- buildffmpeg.sh
+++ buildffmpeg.sh
@@ -4,7 +4,7 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 cd FFmpeg
 
-./configure --prefix=$SCRIPT_DIR/bin --enable-libpulse --enable-nvenc --enable-ffnvcodec
+./configure --prefix=$SCRIPT_DIR/bin --enable-libpulse --enable-nvenc --enable-ffnvcodec --enable-libass
 
 make -j$(nproc)
 make install
